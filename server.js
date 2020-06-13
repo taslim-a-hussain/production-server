b0b248a0686d5bf34aa50a0e71c0d6311e22fc82
taz-sen <taz-sen@outlook.com>
2020-06-13 19:46:48 +1200

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, 'build');
+const port = process.env.PORT || 5000;
+
+app.use(express.static(publicPath));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+
+// Start a server
+app.listen(port, () => {
+    console.log('\x1b[32m','---------- Server is up! ----------');
+    console.log('\x1b[36m%s\x1b[0m','http://localhost:'+port);
+});
